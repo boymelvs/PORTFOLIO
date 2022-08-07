45eed04dcb8cf3ddac2de2f0cae841230bd90e84
Melvin <melvin.enmocino@gmail.com>
2022-08-08 00:24:41 +0800

    updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,32 @@
 "use strict";
 
 const header = document.querySelector(".my-header");
+const sections = document.querySelectorAll("section");
+const project = document.querySelector("#projects");
+const menuItems = document.querySelectorAll(".menu-item");
 
 window.addEventListener("scroll", (e) => {
    window.scrollY > 50 ? header.classList.add("active") : header.classList.remove("active");
+});
+
+// sections.forEach((section) => {
+// });
+const findLocation = project.getBoundingClientRect();
+console.log(findLocation, "findLocation");
+
+console.log(project.offsetTop, "projectoffsetTop");
+console.log(project.offsetHeight, "offsetHeight");
+console.log(window.innerHeight, "innerHeight");
+
+window.addEventListener("scroll", (e) => {
+   if (window.scrollY > findLocation.top && window.scrollY < findLocation.height) {
+      if (findLocation.top < window.innerHeight && findLocation.bottom >= 0) {
+         menuItems[1].classList.add("active");
+         console.log(window.scrollY);
+      }
+
+      return;
+   }
+
+   menuItems[1].classList.remove("active");
 });
